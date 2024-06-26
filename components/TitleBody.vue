397f5d7f37ad47a711881034cafd55eb228ccefc
IamCharlesM <because@charlesisa.dev>
2024-06-26 10:06:38 -0500

    Update Hero component styling

--- components/TitleBody.vue
+++ components/TitleBody.vue
@@ -1,65 +1,42 @@
 <template>
-  <div
-    v-if="heading == 1"
-    class="space-y-3"
-  >
+  <div v-if="heading == 1" class="space-y-3">
     <h1 class="text-4xl font-semibold md:text-5xl">
       {{ title }}
-      <span
-        v-if="name"
-        class="text-primary"
-        >{{ name }}</span
-      >
+      <span v-if="name" class="text-primary">{{ name }}</span>
     </h1>
-    <p class="text-xl text-left md:text-3xl">{{ body }}</p>
+    <p class="text-xl text-left lg:text-center md:text-3xl">{{ body }}</p>
   </div>
   <div v-else-if="heading == 2">
     <h2 class="text-4xl">{{ title }}</h2>
     <p class="text-base">{{ body }}</p>
-    <span
-      v-if="name"
-      class="text-primary"
-      >{{ name }}</span
-    >
+    <span v-if="name" class="text-primary">{{ name }}</span>
   </div>
   <div v-else-if="heading == 3">
     <h3 class="text-3xl">{{ title }}</h3>
     <p class="text-sm">{{ body }}</p>
-    <span
-      v-if="name"
-      class="text-primary"
-      >{{ name }}</span
-    >
+    <span v-if="name" class="text-primary">{{ name }}</span>
   </div>
   <div v-else-if="heading == 4">
     <h4 class="text-2xl">{{ title }}</h4>
     <p class="text-xs">{{ body }}</p>
-    <span
-      v-if="name"
-      class="text-primary"
-      >{{ name }}</span
-    >
+    <span v-if="name" class="text-primary">{{ name }}</span>
   </div>
   <div v-else-if="heading == 5">
     <h5 class="text-xl">{{ title }}</h5>
     <p class="text-xxs">
       {{ body }}
     </p>
-    <span
-      v-if="name"
-      class="text-primary"
-      >{{ name }}</span
-    >
+    <span v-if="name" class="text-primary">{{ name }}</span>
   </div>
 </template>
 
 <script setup lang="ts">
-  const { heading, body, title } = defineProps<{
-    heading?: Number;
-    title?: String;
-    body?: String;
-    name?: String;
-  }>();
+const { heading, body, title } = defineProps<{
+  heading?: Number;
+  title?: String;
+  body?: String;
+  name?: String;
+}>();
 </script>
 
 <style scoped></style>
